00773108c71eddb7fce8a413b48a430ce1fcc8fb
abdulahaddf <abdulahad.df1@gmail.com>
2025-11-11 18:51:30 +0600

    array insertion

--- practice/array.c
+++ practice/array.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 
-
-
-void display(int arr[],int n){
-for (int i = 0; i < n; i++)
+void display(int arr[], int n)
+// traversal
 {
-    printf("%d\n", arr[i] );
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
 }
 
+int indInsertion(int arr[], int size, int element, int capacity, int index)
+{
+    if (size >= capacity)
+    {
+        return -1;
+    }
+    for (int i = size - 1; i >= index; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+    arr[index] = element;
+    return 1;
+};
 
-}
-
-
-int main(){
-    int arr[100] = {1,4,7,9};
-    display(arr,4);
-
+int main()
+{
+    int arr[100] = {1, 4, 5, 7, 9};
+    int size = 5, element = 8, index = 3;
+    display(arr, size);
+    indInsertion(arr, size, element, 100, index);
+    display(arr, 6);
+    printf("%d is the element\n", arr[5]);
 }
