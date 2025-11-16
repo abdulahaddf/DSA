43b942c22faa1b393f768a9ad49de1caf6607662
abdulahaddf <abdulahad.df1@gmail.com>
2025-11-16 22:34:41 +0600

    insert array to linked list and show the data

--- newLinkedList.cpp
+++ newLinkedList.cpp
@@ -15,17 +15,40 @@ class Node{
 
 
 int main(){
-  Node * Head ;
-  Node * Second ;
-  Head = new Node(4);
-  Head-> next = Second;
-
-  Second = new Node(6);
-  Second -> next = NULL;
+  Node * Head;
+  Head = NULL;
+//   Head = new Node(4);
    
-  cout<< Head->data<<endl;
-  cout<< Head->next << "is the pointer value"<<endl;
-  cout<< Second->data<<endl;
-  cout<< Second->next << "is the pointer value"<<endl;
+//   cout<< Head->data;
+//   cout<< Head->next << "is the pointer";
+
+ int arr[] = {1,3,4,5,6,7};
+
+for (int i = 0; i < 6; i++)
+{
+
+
+
+//Check if the linked list is exists
+if(Head == NULL){
+    Head = new Node(arr[i]);
+}
+// If exist
+else{
+Node * temp;
+temp = new Node(arr[i]);
+temp -> next = Head;
+Head = temp;
+}
+
+}
+
+Node *temp = Head;
+while(temp){
+cout<<temp-> data <<" the address of the value is"<< temp-> next<<endl;
+temp = temp->next;
+}
+
+
 
 }
