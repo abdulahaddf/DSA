e535eb4a02416cb73975eeadb60010b19640cb17
abdulahaddf <abdulahad.df1@gmail.com>
2025-11-17 19:04:07 +0600

    InsertionAtAnyPointLinkedList

--- LinkedList/addElementToEndWithRecursionLikedList.cpp
+++ LinkedList/addElementToEndWithRecursionLikedList.cpp
@@ -1,46 +1,153 @@
-# include<iostream>
+// # include<iostream>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node *next;
+
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     };
+// };
+
+// Node * CreateLinkedList(int arr[], int index, int size){
+//     if(index == size){
+//         return NULL;
+
+//     }
+//     Node *temp;
+//     temp = new Node(arr[index]);
+//     temp -> next = CreateLinkedList(arr, index +1, size);    //it will return the address of the new created node that will be stored in the temps next;
+//     return temp;
+// };
+
+// int main(){
+//   Node * Head, * Tail;
+//   Head = Tail = NULL;
+
+//  int arr[] = {1,3,4,5,6,7};
+
+// Head = CreateLinkedList(arr,0,6);
+
+// // Print the value
+// Node *temp = Head;
+// while(temp){
+// cout<<temp-> data <<" the address of the value is"<< temp-> next<<endl;
+// temp = temp->next;
+// }
+
+// }
+
+// Add to starting with recursion
+// #include <iostream>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next;
+
+//     Node(int value)
+//     {
+//         data = value;
+//         next = NULL;
+//     };
+// };
+
+// Node *CreateLinkedList(int arr[], int index, int size, Node *prev)
+// {
+//     if (index == size)
+//     {
+//         return prev;
+//     }
+
+//     Node *temp;
+//     temp = new Node(arr[index]);
+//     temp->next = prev; // it will return the address of the new created node that will be stored in the temps next;
+//     return CreateLinkedList(arr, index + 1, 6, temp);
+
+//     return temp;
+// };
+
+// Add to any particular position with recursion
+#include <iostream>
 using namespace std;
 
-
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
 
-    Node(int value){
+    Node(int value)
+    {
         data = value;
-        next = NULL; 
+        next = NULL;
     };
 };
 
-Node * CreateLinkedList(int arr[], int index, int size){
-    if(index == size){
+Node *CreateLinkedList(int arr[], int index, int size)
+{
+    if (index == size)
+    {
         return NULL;
-
     }
     Node *temp;
     temp = new Node(arr[index]);
-    temp -> next = CreateLinkedList(arr, index +1, size);    //it will return the address of the new created node that will be stored in the temps next;
+    temp->next = CreateLinkedList(arr, index + 1, size); // it will return the address of the new created node that will be stored in the temps next;
     return temp;
 };
 
 
-int main(){
-  Node * Head, * Tail;
-  Head = Tail = NULL;
 
+Node* insertAtPosition(Node* head, int x, int value) {
+    // Insert at head
+    if (x == 0) {
+        Node* temp = new Node(value);
+        temp->next = head;
+        return temp;
+    }
 
- int arr[] = {1,3,4,5,6,7};
+    Node* curr = head;
+    for (int i = 0; curr != NULL && i < x - 1; i++) {
+        curr = curr->next;
+    }
 
-Head = CreateLinkedList(arr,0,6);
+    if (curr == NULL) return head;  // invalid position
 
+    Node* temp = new Node(value);
+    temp->next = curr->next;
+    curr->next = temp;
 
-
-// Print the value
-Node *temp = Head;
-while(temp){
-cout<<temp-> data <<" the address of the value is"<< temp-> next<<endl;
-temp = temp->next;
+    return head;
 }
 
+
+int main()
+{
+    Node *Head, *Tail;
+    Head = Tail = NULL;
+
+    int arr[] = {1, 3, 4, 5, 6, 7};
+
+    Head = CreateLinkedList(arr, 0, 6);
+
+    int x = 2;
+    int value = 30;
+
+   Head = insertAtPosition(Head, x, value);
+
+
+
+
+    // Print the value
+    Node *temp = Head;
+    while (temp)
+    {
+        cout << temp->data << " the address of the value is" << temp->next << endl;
+        temp = temp->next;
+    }
 }
