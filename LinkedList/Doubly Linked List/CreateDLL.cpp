946c0ee783563560db1187b65b37c4916a9847fe
abdulahaddf <abdulahad.df1@gmail.com>
2025-11-18 20:17:47 +0600

    DLL insertion at any point

--- LinkedList/Doubly Linked List/CreateDLL.cpp
+++ LinkedList/Doubly Linked List/CreateDLL.cpp
@@ -16,7 +16,7 @@ public:
     }
 };
 
-Node *createDDL(int arr[], int index, int size)
+Node *createDLL(int arr[], int index, int size)
 {
     if (index == size)
     {
@@ -24,12 +24,16 @@ Node *createDDL(int arr[], int index, int size)
     }
 
     Node *temp = new Node(arr[index]);
-    temp->next->prev = temp->next;
-    temp->next = createDDL(arr, index + 1, size);
+    temp->next = createDLL(arr, index + 1, size);
+    // temp->next->prev = temp;
+      // if there is a next node, set its prev to temp
+    if (temp->next != nullptr) {
+        temp->next->prev = temp;
+    }
     return temp;
 }
 
-Node *CreateDDLbyTail(int arr[], int size)
+Node *CreateDLLbyTail(int arr[], int size)
 {
     Node *Head = new Node(arr[0]);
     Node *Tail = Head;
@@ -44,25 +48,49 @@ Node *CreateDDLbyTail(int arr[], int size)
     return Head;
 }
 
-void InsertAt(Node * &head, int arr[], int size, int psn, int num)
-{
-    Node *temp = head;
-    cout << temp << endl;
-    Node *toInsert = new Node(num);
-    for (int i = 0; i < psn; i++)
-    {
-        if (i == psn - 1)
-        {
+// 
 
-            toInsert->next = temp->next;
-            temp->next->prev = toInsert;
-            temp->next = toInsert;
-            toInsert->prev = temp;
-         
-        }
-        temp = temp->next;
+void InsertAt(Node *& head, int psn, int num) {
+    Node* toInsert = new Node(num);
+
+    if (psn <= 0 || head == NULL) {
+        toInsert->next = head;
+        toInsert->prev = NULL;
+        if (head) head->prev = toInsert;
+        head = toInsert;
+        return;
     }
-};
+
+    // Insert at head (position 0)
+    if (psn <= 0 || head == nullptr) {
+        toInsert->next = head;
+        toInsert->prev = nullptr;
+        if (head) head->prev = toInsert;
+        head = toInsert;
+        return;
+    }
+
+    // Walk to node at position (psn - 1) or last node if list shorter
+    Node* temp = head;
+    int i = 0;
+    while (temp->next != nullptr && i < psn - 1) {
+        temp = temp->next;
+        ++i;
+    }
+
+    // Now temp is either:
+    // - node at index (psn-1) (normal case), or
+    // - the last node (if psn > length), we'll append after temp
+
+    Node* next = temp->next;       // maybe nullptr if temp is tail
+
+    // Link the new node between temp and next
+    temp->next = toInsert;
+    toInsert->prev = temp;
+    toInsert->next = next;
+    if (next) next->prev = toInsert;
+}
+
 
 int main()
 {
@@ -70,10 +98,10 @@ int main()
     int arr[] = {1, 2, 3, 4, 5,6,7,8,9};
     int size = 9;
 
-    // head = createDDL(arr,0,size);
-    head = CreateDDLbyTail(arr, size);
+    head = createDLL(arr,0,size);
+    // head = CreateDLLbyTail(arr, size);
 
-    InsertAt(head, arr, size, 3, 30);
+    InsertAt(head, 0, 30);
 
     // Preview the DDL
     Node *temp = head;
