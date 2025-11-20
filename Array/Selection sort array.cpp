8ed6ed7a39759eea417347bd17ff0de38cf57dd3
abdulahaddf <abdulahad.df1@gmail.com>
2025-11-21 01:02:19 +0600

    selection sort in array

---
+++ Array/Selection sort array.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<n-1; i++){
+        int minIndex = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+
+}
