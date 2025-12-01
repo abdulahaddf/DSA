2ee72df176b3715ae1378c26dfb59f6101846f4a
abdulahaddf <abdulahad.df1@gmail.com>
2025-12-01 12:05:49 +0600

    Search in 2d array

--- 2dArray/SearchIn2dArray.cpp
+++ 2dArray/SearchIn2dArray.cpp
@@ -3,6 +3,7 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
+
         int rowIndex =0;
         int colIndex = col -1;
 
