95a07d46f7a3e6ce9286e309f01dbd33adc58c86
abdulahaddf <abdulahad.df1@gmail.com>
2026-02-21 02:05:14 +0600

    watermelon problem

--- CodeForces/cp.cpp
+++ CodeForces/cp.cpp
@@ -1,1 +1,14 @@
-#incl
+#include <iostream>
+using namespace std;
+
+// watermelon problem
+
+int main(){
+    int w;
+    cin>>w;
+    if(w%2==0 && w>2){
+        cout<<"YES";
+    }
+    else cout<<"NO";
+
+}
